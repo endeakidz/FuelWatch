ddeaddee33fdec9ceace823a86f1faa7b4b71d3c
Thien Quang Trinh <billytrinh95@gmail.com>
2017-11-02 16:26:37 +0800

    Version 2

--- fuelwatch2.py
+++ fuelwatch2.py
@@ -10,9 +10,9 @@ region = [25, 26]
 tomorrow = ['', '&Day=tomorrow']
 
 links = [
-         "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={}&Region={}{}".format(*i)
-         for i in itertools.product(product, region, tomorrow)
-         ]
+         "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={" \
+         "}&Region={}{}".format(
+                                *i) for i in itertools.product(product, region, tomorrow)]
 
 item_properties = []
 
@@ -21,23 +21,32 @@ for link in links:
     items = content.findall('.//item')
     
     for item in items:
-        item_properties.append(
-                               {'brand': item.find('./brand').text,
+        item_properties.append({
+                               'brand': item.find('./brand').text,
                                'price': float(item.find('./price').text),
                                'location': item.find('./location').text,
                                'address': item.find('./address').text,
-                               'tomorrow': True if datetime.strptime(item.find('./date').text,'%Y-%m-%d').date() != date.today() else False})
+                               'tomorrow': True if datetime.strptime(item.find('./date').text,
+                                                                     '%Y-%m-%d').date() !=
+                               date.today() else False
+                               })
 
-new_item_properties = sorted(item_properties, key=itemgetter('price', 'brand', 'address'))
+new_item_properties = sorted(item_properties,
+                             key=itemgetter('price', 'brand', 'address'))
 
 current_date_time = datetime.strftime(datetime.now(), '%d/%m/%Y %I:%M:%S %p')
 
 table_string = ""
 for item in new_item_properties:
     if item['tomorrow'] == False:
-        table_string += "<tr><td>{price}</td><td>{location}</td><td>{address}</td><td>{brand}</td></tr>".format(**item)
+        table_string += "<tr><td>{price}</td><td>{location}</td><td>{" \
+            "address}</td><td>{brand}</td></tr>".format(
+                                                        **item)
     else:
-        table_string += '<tr class="highlight"><td>{price}</td><td>{location}</td><td>{address}</td><td>{brand}</td></tr>'.format(**item)
+        table_string += '<tr class="highlight"><td>{price}</td><td>{' \
+            'location}</td><td>{address}</td><td>{' \
+                'brand}</td></tr>'.format(
+                                          **item)
 
 style = '''
     <style style="text/css">
@@ -48,8 +57,10 @@ style = '''
     '''
 
 header = '98 RON for NORTH RIVER and SOUTH RIVER'
-template = '<!DOCTYPE html>' + style + '<html><head>{}</head><head style="background-color:#00FFFF">{}</head><body><table>{}</table></body></html>'.format(current_date_time, header, table_string)
+template = '<!DOCTYPE html>' + style + '<html><head>{}</head><head '\
+    'style="background-color:#00FFFF">{'\
+        '}</head><body><table>{}</table></body></html>'.format(
+                                                               current_date_time, header, table_string)
 
 with open("./output.html", "w") as f:
     f.write(template)
-
