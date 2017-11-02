75baeddc179cb96ae4f2aea653ce30f951f9780b
Thien Quang Trinh <billytrinh95@gmail.com>
2017-11-02 16:19:19 +0800

    Improved Version

--- fuelwatch2.py
+++ fuelwatch2.py
@@ -4,43 +4,44 @@ from datetime import datetime, date
 from operator import itemgetter
 import itertools
 
-
 # Parameters list
-product = [2]
-region = [25,26]
-links = []
-for i in itertools.product(product,region):
-    links.append("http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={}&Region={}".format(*i))
-    links.append("http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={}&Region={}&Day=tomorrow".format(*i))
+product = [6]
+region = [25, 26]
+tomorrow = ['', '&Day=tomorrow']
 
-list = []
+links = [
+         "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={}&Region={}{}".format(*i)
+         for i in itertools.product(product, region, tomorrow)
+         ]
+
+item_properties = []
 
 for link in links:
     content = etree.fromstring(urlopen(link).read())
     items = content.findall('.//item')
     
     for item in items:
-        list.append(
-                    {'brand': item.find('./brand').text,
-                    'price': float(item.find('./price').text),
-                    'location': item.find('./location').text,
-                    'address': item.find('./address').text,
-                    'tomorrow': True if datetime.strptime(item.find('./date').text, '%Y-%m-%d').date() != date.today() else False})
+        item_properties.append(
+                               {'brand': item.find('./brand').text,
+                               'price': float(item.find('./price').text),
+                               'location': item.find('./location').text,
+                               'address': item.find('./address').text,
+                               'tomorrow': True if datetime.strptime(item.find('./date').text,
+                                                                     '%Y-%m-%d').date() != date.today() else False})
 
-new_list = sorted(list, key=itemgetter('price', 'brand', 'address'))
+new_item_properties = sorted(item_properties, key=itemgetter('price', 'brand', 'address'))
 
-for item in new_list:
+for item in new_item_properties:
     print('{price:.2f} {location:20s}Address: {address:50s}{brand:20s}{tomorrow:5b}'.format(**item))
 
 current_date_time = datetime.strftime(datetime.now(), '%d/%m/%Y %I:%M:%S %p')
 
 table_string = ""
-for item in new_list:
+for item in new_item_properties:
     if item['tomorrow'] == False:
         table_string += "<tr><td>{price}</td><td>{location}</td><td>{address}</td><td>{brand}</td></tr>".format(**item)
     else:
-        table_string += '<tr class="highlight"><td>{price}</td><td>{location}</td><td>{address}</td><td>{brand}</td></tr>'.format(
-                                                                                                                                  **item)
+        table_string += '<tr class="highlight"><td>{price}</td><td>{location}</td><td>{address}</td><td>{brand}</td></tr>'.format(**item)
 
 style = '''
     <style style="text/css">
@@ -52,10 +53,7 @@ style = '''
 
 header = '98 RON for NORTH RIVER and SOUTH RIVER'
 template = '<!DOCTYPE html>' + style + '<html><head>{}</head><head style="background-color:#00FFFF">{}</head><body><table>{}</table></body></html>'.format(
-                                                                                                                                                           current_date_time, header,
-                                                                                                                                                           table_string)
+                                                                                                                                                           current_date_time, header, table_string)
 
 with open("./output.html", "w") as f:
     f.write(template)
-
-# template = "<html>" + "<head>" + "</head" + "body"
